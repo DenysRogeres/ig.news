767b0300ac788e953650658623fa028f09775383
DenysRogeres <dennysrogeres@gmail.com>
2022-03-11 00:48:13 -0300

    adding styles

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,16 +1,12 @@
 import Head from "next/head";
 
-import styled from "../styles/home.module.scss";
-
 export default function Home() {
   return (
     <>
       <Head>
         <title>ig.news</title>
       </Head>
-      <h1 className={styled.title}>
-        Hello <span className={styled.span}>Word</span>
-      </h1>
+      <h1>Hello Word</h1>
     </>
   )
 }
